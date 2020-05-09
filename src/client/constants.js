836b4e6f7646466b2331bb309dcb374a33f2a4c9
ck <ck@reviewshare.io>
2020-05-09 16:45:20 +0900

    [Modified] prefix 수정

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,1 +1,1 @@
-export const PREFIX = process.env.NODE_ENV === "production" ? "http://ck-studios.github.io/bingo-project" : "";
+export const PREFIX = process.env.NODE_ENV === "production" ? "" : "";
