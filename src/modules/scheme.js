8cb9d325962b3a08ce6264297304ebe4cafcaf32
ck <ck@reviewshare.io>
2020-07-31 22:45:35 +0900

    [update] .

--- src/modules/scheme.js
+++ src/modules/scheme.js
@@ -28,6 +28,29 @@ export const LOAD_BINGO = gql`
   }
 `;
 
+export const GET_MATCHED_BINGO = gql`
+  query bingo($id: ID!) {
+    bingo(id: $id) {
+      id
+      title
+      thumbnail
+      text
+      boardTheme {
+            id
+            size
+            boardImage
+            ringImage
+          }
+          bingoResults {
+            id
+            type
+            text
+            image
+          }
+    }
+  }
+`;
+
 export const LOAD_LOCAL_BINGO = gql`
     {
         allBingos @client {
--- src/pages/bingo/index.js
+++ src/pages/bingo/index.js
@@ -4,15 +4,32 @@ import AnimationFrame from "common/animation/AnimationFrame";
 import {initializeStore} from "client/store";
 import {initializeApollo} from "client/client";
 import Head from "next/head"
+import {useRouter} from "next/router";
 import {BASE_URL} from "client/constants";
+import {GET_MATCHED_BINGO, LOAD_BINGO} from "modules/scheme";
 
 export default function Index(props) {
   // const game = props?.initialReduxState?.bingo.selectedGame;
   //
   // console.log("game::: ", game);
+  // router.query.id
+  console.log("matched:: ", props.matchedBingo);
+  const matchedGame = props.matchedBingo;
+
+  const router = useRouter();
   return (
     <>
-
+      <Head>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@빙고링"/>
+        <meta name="twitter:creator" content="@빙고링"/>
+        <meta property="og:url" content={BASE_URL + "/bingo?id=" + matchedGame?.id}/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content={matchedGame?.title}/>
+        <meta property="og:description" content="빙고링"/>
+        <meta property="og:image" content={matchedGame?.thumbnail}/>
+        <meta property="og:app_id" content="1015774698842581"/>
+      </Head>
       <ContainerLayout>
         <AnimationFrame>
           <BingoContainer/>
@@ -23,12 +40,18 @@ export default function Index(props) {
 }
 
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async (context) => {
   const reduxStore = initializeStore();
   const apolloClient = initializeApollo();
+  const matchedBingo = await apolloClient.query({
+    query: GET_MATCHED_BINGO,
+    variables: {id: context.query.id}
+  });
+
 
   return {
     initialReduxState: reduxStore.getState(),
-    initialApolloState: apolloClient.cache.extract()
+    initialApolloState: apolloClient.cache.extract(),
+    matchedBingo
   }
 };
