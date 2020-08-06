e857d9909f8b3b13c73d5f4984dcd364797b97cc
ck <ck@devhi.me>
2020-08-06 15:20:50 +0900

    [Update] 사이트 meta 태그 설정

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,26 @@ import ContainerLayout from "common/components/layout/ContainerLayout";
 import AnimationFrame from "common/animation/AnimationFrame";
 import {pointColor} from "common/theme/theme";
 import {initializeApollo} from "client/client";
+import Head from "next/head";
 
 export default function Index() {
-  return (
-      <ContainerLayout>
-        <AnimationFrame>
-          <MainContainer/>
-        </AnimationFrame>
-      </ContainerLayout>
-  )
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content="빙고링"/>
+                <meta property="og:description" content="인기있는 빙고 모음"/>
+                <meta property="og:site_name" content="빙고링"/>
+            </Head>
+            <ContainerLayout>
+                <AnimationFrame>
+                    <MainContainer/>
+                </AnimationFrame>
+            </ContainerLayout>
+        </>
+    )
 }
 
-Index.getInitialProps = async() => {
+Index.getInitialProps = async () => {
     const apolloClient = initializeApollo();
 
 
